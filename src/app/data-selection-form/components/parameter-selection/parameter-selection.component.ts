792d8ad5e7256f22e932155dcc7f6415c587ac73
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2025-03-06 10:39:06 +0100

    PR fixes #31

--- src/app/data-selection-form/components/parameter-selection/parameter-selection.component.ts
+++ src/app/data-selection-form/components/parameter-selection/parameter-selection.component.ts
@@ -1,7 +1,6 @@
 import {AsyncPipe} from '@angular/common';
 import {Component, inject, input, output} from '@angular/core';
 import {Store} from '@ngrx/store';
-import {Language} from '../../../shared/models/language';
 import {ParameterGroup} from '../../../shared/models/parameter';
 import {TranslatableStringPipe} from '../../../shared/pipes/translatable-string.pipe';
 import {appFeature} from '../../../state/app/reducers/app.reducer';
@@ -19,7 +18,7 @@ export class ParameterSelectionComponent {
   public readonly parameterGroup = input.required<ParameterGroup>();
   public readonly isSelected = input(false);
   public readonly selected = output<string | null>();
-  protected readonly currentLanguage$ = this.store.select<Language>(appFeature.selectLanguage);
+  protected readonly currentLanguage$ = this.store.select(appFeature.selectLanguage);
 
   protected setParameterGroupSelected(): void {
     const parameterGroupId = this.isSelected() ? null : this.parameterGroup().id;
