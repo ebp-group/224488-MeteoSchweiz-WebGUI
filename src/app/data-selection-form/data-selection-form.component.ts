d2182c31a4a38e7d73d802b9ad1c3f2d71b0ac51
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2025-03-28 10:19:18 +0100

    Use BEM and general design clean up #48
    This change introduces BEM to structure the SCSS files. Firstly, it
    makes it easier to identify the corresponding class usage in the
    templates (e.g. using the CTRL-click) as it is unique afterward. And
    secondly, it makes refactorings (especially renamings) a lot less
    dangerous as the IDE sometimes does rename a lot more than necessary.
    
    The whole parameter group and station selection part is now a lot closer
    to the desired design. The debug options to switch the language and
    throw an error is removed.

--- src/app/data-selection-form/data-selection-form.component.ts
+++ src/app/data-selection-form/data-selection-form.component.ts
@@ -5,19 +5,14 @@ import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-to
 import {MatStepperModule} from '@angular/material/stepper';
 import {TranslocoModule} from '@jsverse/transloco';
 import {Store} from '@ngrx/store';
-import {MapContainerComponent} from '../map/components/map-container/map-container.component';
 import {MeasurementDataType} from '../shared/models/measurement-data-type';
-import {appActions} from '../state/app/actions/app.actions';
 import {formActions} from '../state/form/actions/form.actions';
 import {formFeature} from '../state/form/reducers/form.reducer';
-import {CollectionSelectionComponent} from './components/collection-selection/collection-selection.component';
 import {DownloadAssetComponent} from './components/download-asset/download-asset.component';
 import {IntervalSelectionComponent} from './components/interval-selection/interval-selection.component';
-import {ParameterGroupSelectionComponent} from './components/parameter-group-selection/parameter-group-selection.component';
 import {SelectionReviewComponent} from './components/selection-review/selection-review.component';
-import {StationSelectionComponent} from './components/station-selection/station-selection.component';
+import {StationSelectionStepComponent} from './components/station-selection-step/station-selection-step.component';
 import {TimeRangeSelectionComponent} from './components/time-range-selection/time-range-selection.component';
-import type {Language} from '../shared/models/language';
 
 @Component({
   selector: 'app-data-selection-form',
@@ -32,10 +27,7 @@ import type {Language} from '../shared/models/language';
     DownloadAssetComponent,
     MatStepperModule,
     AsyncPipe,
-    MapContainerComponent,
-    StationSelectionComponent,
-    CollectionSelectionComponent,
-    ParameterGroupSelectionComponent,
+    StationSelectionStepComponent,
   ],
   templateUrl: './data-selection-form.component.html',
   styleUrl: './data-selection-form.component.scss',
@@ -47,14 +39,6 @@ export class DataSelectionFormComponent {
   protected readonly selectedSelectedTimeRange$ = this.store.select(formFeature.selectSelectedTimeRange);
   protected readonly selectedMeasurementDataType$ = this.store.select(formFeature.selectSelectedMeasurementDataType);
   protected readonly selectedCollection$ = this.store.select(formFeature.selectSelectedCollection);
-
-  protected changeLanguage(language: Language): void {
-    this.store.dispatch(appActions.setLanguage({language}));
-  }
-
-  protected testError(): void {
-    throw new Error('Test');
-  }
 
   protected setSelectedMeasurementDataType(measurementDataType: MeasurementDataType): void {
     this.store.dispatch(formActions.setSelectedMeasurementDataType({measurementDataType}));
