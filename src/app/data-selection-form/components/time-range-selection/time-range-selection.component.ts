4ace6208f63ad0ecb90322c89f872282f903256c
Josua Cantieni <josua.cantieni@ebp.ch>
2025-04-01 16:50:44 +0200

    Style time-range selection #81

--- src/app/data-selection-form/components/time-range-selection/time-range-selection.component.ts
+++ src/app/data-selection-form/components/time-range-selection/time-range-selection.component.ts
@@ -1,5 +1,8 @@
 import {AsyncPipe, DatePipe} from '@angular/common';
 import {Component, inject} from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatTooltipModule} from '@angular/material/tooltip';
 import {TranslocoModule} from '@jsverse/transloco';
 import {Store} from '@ngrx/store';
 import {IncludesPipe} from '../../../shared/pipes/includes.pipe';
@@ -11,7 +14,7 @@ import type {TimeRange} from '../../../shared/models/time-range';
 
 @Component({
   selector: 'app-time-range-selection',
-  imports: [TranslocoModule, AsyncPipe, DatePipe, IncludesPipe],
+  imports: [TranslocoModule, AsyncPipe, DatePipe, IncludesPipe, MatRadioModule, MatIcon, MatTooltipModule],
   templateUrl: './time-range-selection.component.html',
   styleUrl: './time-range-selection.component.scss',
 })
@@ -22,7 +25,11 @@ export class TimeRangeSelectionComponent {
   protected readonly availableTimeRanges$ = this.store.select(selectAvailableTimeRanges);
   protected readonly historicalDateRanges$ = this.store.select(selectSortedHistoricalDateRanges);
 
-  protected setSelectedTimeRange(timeRange: TimeRange, historicalDateRange: DateRange | null): void {
-    this.store.dispatch(formActions.setSelectedTimeRange({timeRange, historicalDateRange}));
+  protected setSelectedTimeRange(timeRange: TimeRange | DateRange): void {
+    if (typeof timeRange === 'string') {
+      this.store.dispatch(formActions.setSelectedTimeRange({timeRange, historicalDateRange: null}));
+    } else {
+      this.store.dispatch(formActions.setSelectedTimeRange({timeRange: 'historical', historicalDateRange: timeRange}));
+    }
   }
 }
