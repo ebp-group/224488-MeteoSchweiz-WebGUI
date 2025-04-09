5afbdb5463a1991966010f47019fa8a9ae591926
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2025-04-09 09:52:41 +0200

    PR feedback #84

--- src/app/data-selection-form/components/collection-selection/collection-selection.component.ts
+++ src/app/data-selection-form/components/collection-selection/collection-selection.component.ts
@@ -1,5 +1,6 @@
 import {AsyncPipe} from '@angular/common';
 import {Component, inject} from '@angular/core';
+import {MatRipple} from '@angular/material/core';
 import {MatRadioButton} from '@angular/material/radio';
 import {TranslocoModule} from '@jsverse/transloco';
 import {Store} from '@ngrx/store';
@@ -12,7 +13,7 @@ import {StationInfoComponent} from '../station-info/station-info.component';
 
 @Component({
   selector: 'app-collection-selection',
-  imports: [AsyncPipe, TranslatableStringPipe, TranslocoModule, MatRadioButton, StationInfoComponent, StationInfoComponent],
+  imports: [AsyncPipe, TranslatableStringPipe, TranslocoModule, MatRadioButton, StationInfoComponent, MatRipple],
   templateUrl: './collection-selection.component.html',
   styleUrl: './collection-selection.component.scss',
 })
