1fe5e374627b4fe4e534d94eadb0e31c2f7ccbb9
Josua Cantieni <josua.cantieni@ebp.ch>
2025-04-15 18:13:34 +0200

    Change formate for historical date ranges #41
    The format for historical date ranges was changed to `YYYY-YYYY` in the
    csv filename. For this the parsing had to be adjusted.
    
    We opted to keep the date objects in place. There is a chance that month
    and day will be reintroduced at a later stage so we want to keep the
    option for that.
    
    Some tests were changed to not test for correct date transformation
    anymore. These tests test functions which rely on functions from
    `date-transformation.utils.ts`. These functions are already tested and
    test that transforming string to date and back works. This means that
    there is no need to test this again in the other tests and can be
    assumed working. This reduces the number of test that need to be
    adjusted when the format next changes.

--- src/app/shared/utils/date-transformation.utils.ts
+++ src/app/shared/utils/date-transformation.utils.ts
@@ -3,26 +3,21 @@
  */
 export function transformDateToString(date: Date): string {
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // months are 0-indexed
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}${month}${day}`;
+  return `${year}`;
 }
 
 /**
  * Transforms a string in the format 'YYYYMMDD' into a date object; returns undefined if the input is either undefined or incorrectly formatted.
  */
 export function transformStringToDate(date: string | undefined): Date | undefined {
-  if (!date || date.length < 8) {
+  if (!date || date.length < 4) {
     return undefined;
   }
   const year = Number(date.substring(0, 4));
-  const month = Number(date.substring(4, 6));
-  const day = Number(date.substring(6, 8));
 
-  if (Number.isNaN(year) || Number.isNaN(month) || Number.isNaN(day)) {
+  if (Number.isNaN(year)) {
     return undefined;
   }
 
-  // Note: months are 0-indexed in JavaScript Date objects
-  return new Date(year, month - 1, day);
+  return new Date(date);
 }
